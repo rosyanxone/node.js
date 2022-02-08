bf01261d278c1ea70411362447f038e2a59ee60d
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-02-08 18:37:53 +0800

    first commit

---
+++ coba.js
@@ -0,0 +1,4 @@
+// console.log('Hello World');
+const functionONE = (nama) => `Hi, nama saya ${nama}`;
+
+module.exports = functionONE;
---
+++ index.js
@@ -0,0 +1,5 @@
+// const functionONE = (nama) => `Hi, nama saya ${nama}`;
+// console.log(functionONE('Rausyanfikr'));
+functionONE = require('./coba.js');
+
+console.log(functionONE('Rosyan'));

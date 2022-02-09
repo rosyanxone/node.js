92f1190acf43d1e0c2ddabd43c386925fcc3687d
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-02-09 08:16:44 +0800

    5th commit

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -22,7 +22,47 @@ const fs = require('fs');
 
 
 // Membaca File (Asynchronous)
-fs.readFile('data/test.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
+// fs.readFile('data/test.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+// Readline
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// rl.question('Masukkan nama anda : ', (nama) => {
+//     rl.question('Masukkan nohp anda : ', (nohp) => {
+//         console.log(`Terimakasih ${nama}`);
+//         console.log(`No Hp anda : ${nohp}`);
+//         rl.close();
+//     });
+// });
+
+
+
+// Contact
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+rl.question('Masukkan nama anda : ', (nama) => {
+    rl.question('Masukkan noHp anda : ', (noHP) => {
+        const contacts = { nama, noHP };
+        const file = fs.readFileSync('data/contact.json', 'utf-8');
+        const contact = JSON.parse(file);
+
+        contact.push(contacts)
+
+        fs.writeFileSync('data/contact.json', JSON.stringify(contact));
+        console.log(`Terimakasih ${nama}, sudah memasukkan nomor ${noHP}`);
+        rl.close();
+    });
 });
